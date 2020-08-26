921353d6f37319a2fc65bd3e18076e7b43824cfb
Jaron Degen <jaron.degen90@gmail.com>
2020-08-26 22:37:47 +0200

    Phase 2 completion

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
-
+const csrf = require("csurf");
 const app = express();
 const port = process.env.PORT || 3000;
+const cookieParser = require("cookie-parser");
 
 app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
+
+const csurfProtection = csrf({cookie: true});
 
 const users = [
   {
@@ -14,13 +19,25 @@ const users = [
   }
 ];
 
-
 app.get("/", (req, res) => {
   // res.send("Hello World!");
   res.render('index', { title: 'Existing Users',users });
 });
 
+app.get("/create", csurfProtection, (req, res) => {
+  res.render("create", { title: "Create User", users, csrfToken: req.csrfToken() })
+});
 
+app.post("/create", csurfProtection, (req, res) => {
+  console.log(req.body)
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+
+  const user = { id:users.length+1, firstName, lastName, email, password, confirmedPassword };
+  // req.body.id = users.length + 1;
+  users.push(user);
+  res.redirect("/")
+
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
